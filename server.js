7c937c7eed06f4e37899705453c644b586619a95
Daniel Bonn <danieltbonn@gmail.com>
2023-10-12 17:18:20 -0500

    added database name to my connection.js file

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-
-const dbName = 'socialnetworkDB';
-
 db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
