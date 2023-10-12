5a8c4b9de8304632ee7674d485b23012dbeb95cc
Daniel Bonn <danieltbonn@gmail.com>
2023-10-11 21:03:24 -0500

    added basic functionality for adding a user schema

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+// const users = require('./user-routes.js');
+// const thoughts = require('./thought-routes.js')
+
+// router.use('api/users', users);
+// router.use('api/thoughts', thoughts);
+
+
+module.exports = router;
