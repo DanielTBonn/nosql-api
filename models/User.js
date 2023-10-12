0f4612b19559f9a5fe1036e7596a6631a190f256
Daniel Bonn <danieltbonn@gmail.com>
2023-10-11 23:25:29 -0500

    added to User model

--- models/User.js
+++ models/User.js
@@ -1,9 +1,33 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: false },
-    thoughts: String,
-    friends: Number,
+    username: { 
+        type: String,
+        unique: true,
+        required: true, 
+    },
+    email: { 
+        type: String, 
+        required: true 
+    },
+    thoughts: Array,
+    friends: Array,
     lastAccessed: { type: Date, default: Date.now },
   });
+
+const User = mongoose.model('User', userSchema);
+
+const handleError = (err) => console.log(err);
+
+User
+  .create({
+    username: "Daniel Bonn",
+    email: "danieltbonn@gmail.com",
+    thoughts: [],
+    friends: [],
+  })
+  .then(result => console.log('Created a new document', result))
+  .catch(err => handleError(err));
+
+
+module.exports = User;
