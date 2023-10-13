958f488327d302259971e594f73c87f2e8fdb878
Daniel Bonn <danieltbonn@gmail.com>
2023-10-12 19:29:01 -0500

    created the reaction schema

--- models/User.js
+++ models/User.js
@@ -1,15 +1,37 @@
 const mongoose = require('mongoose');
+const { ObjectId } = require('mongodb') 
 
-
-
-const thoughtSchema = new mongoose.Schema({
-  thoughtText: {
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: ObjectId,
+    default: new ObjectId(),
+  },
+  reactionText: {
+    type: String,
+    required: true,
+    
+  },
+  username: {
     type: String,
     required: true,
   },
   createdAt: {
     type: Date,
     default: Date.now,
+
+  },
+})
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    
   },
   username: {
     type: String,
