cf85e8e54e1d829e97a52183ab4e67e545ede268
Daniel Bonn <danieltbonn@gmail.com>
2023-10-16 20:40:57 -0500

    added more boilerpalte to get routes middleware setup and controller structure outlined

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./config/connection');
 const User = require('./models/User');
 const Thought = require('./models/Thought');
+const routes = require('./routes')
 
 
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
